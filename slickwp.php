9aabd68cd5522c270541df90d6df5356e7ceedd9
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 10:12:10 -0300

    test

--- slickwp.php
+++ slickwp.php
@@ -12,6 +12,15 @@ Text Domain:  wporg
 Domain Path:  /languages
 */
 
+function myplugin_scripts() {
+    wp_register_style( 'slckwpGs',  plugin_dir_url( __FILE__ ) . 'slick/slick/slick.css' );
+    wp_register_style( 'slckwpGsTheme',  plugin_dir_url( __FILE__ ) . 'slick/slick/slick-theme.css',array('slickwpGs') );
+    wp_enqueue_script('slick_wp_gs_ms', plugin_dir_url(__FILE__).'slick/slick/slick.js', array('jquery'), 'version', true);
+    wp_enqueue_style( 'slckwpGsTheme' );
+    wp_enqueue_script('slick_wp_gs_ms');
+}
+add_action( 'wp_enqueue_scripts', 'myplugin_scripts' );
+
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 if (!defined('SLICKWP_FILE')) {
     define('SLICKWP_FILE', __FILE__);
