bc700ca4146cfc502a03fd61fc103c8c6766a8e9
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 10:24:33 -0300

    adding atts to shortcode and testing

--- src/Singleton.php
+++ src/Singleton.php
@@ -4,7 +4,6 @@ class Singleton{
 	private static $_instance = null;
 	public static function instance()
 	{
-		die(var_dump('kdmclsm'));
 		if (is_null(self::$_instance)) {
 			self::$_instance = new self();
 		}
