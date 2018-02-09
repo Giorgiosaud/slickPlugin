72c21a04499d59787a3ce7594ae297c45201d53f
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 10:05:13 -0300

    test

--- slickwp.php
+++ slickwp.php
@@ -11,6 +11,13 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  wporg
 Domain Path:  /languages
 */
+
+function myplugin_scripts() {
+    wp_register_style( 'foo-styles',  plugin_dir_url( __FILE__ ) . 'assets/foo-styles.css' );
+    wp_enqueue_style( 'foo-styles' );
+}
+add_action( 'wp_enqueue_scripts', 'myplugin_scripts' );
+
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 if (!defined('SLICKWP_FILE')) {
     define('SLICKWP_FILE', __FILE__);
@@ -22,10 +29,5 @@ require_once 'vendor/autoload.php';
 function slickPlugin()
 {
     return \giorgiosaud\slickwp\Initializers::instance();
-}
-add_action( 'wp_enqueue_scripts', 'loadstyles' );
-function loadstyles(){
-		wp_register_style( 'slickWpGs', plugins_url( '/slick/slick/slick.css', __FILE__ ), array('jquery'), '1.8.0', 'all' );
-		wp_enqueue_style( 'slickWpGs');
 }
 $GLOBALS['slickwp'] = slickPlugin();
