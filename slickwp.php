d2dfeb13c1f76f3c0047c8525725e19dcd5e260e
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 10:02:47 -0300

    test

--- slickwp.php
+++ slickwp.php
@@ -23,5 +23,9 @@ function slickPlugin()
 {
     return \giorgiosaud\slickwp\Initializers::instance();
 }
-
+add_action( 'wp_enqueue_scripts', array('loadstyles') );
+function loadstyles(){
+		wp_register_style( 'slickWpGs', plugins_url( '/slick/slick/slick.css', __FILE__ ), array('jquery'), '1.8.0', 'all' );
+		wp_enqueue_style( 'slickWpGs');
+}
 $GLOBALS['slickwp'] = slickPlugin();
