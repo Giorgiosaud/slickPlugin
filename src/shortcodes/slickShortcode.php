9bfe3141aa1c4b66afc2f707bbbd84b5fb319b51
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 13:17:43 -0300

    debug

--- src/shortcodes/slickShortcode.php
+++ src/shortcodes/slickShortcode.php
@@ -179,6 +179,7 @@ class slickShortcode extends Singleton{
 		
 			);
 		var_dump($args);
+		wp_reset_query();
 		$query = new \WP_Query( $args );
 		while ($query->have_posts() ) : $query->the_post();
 			$post=array();
