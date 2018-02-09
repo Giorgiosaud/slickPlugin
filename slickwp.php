7a13dbca2b5a2551fbae3f942af56cf84f45b46a
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 10:12:54 -0300

    add css and js

--- slickwp.php
+++ slickwp.php
@@ -14,7 +14,7 @@ Domain Path:  /languages
 
 function myplugin_scripts() {
     wp_register_style( 'slckwpGs',  plugin_dir_url( __FILE__ ) . 'slick/slick/slick.css' );
-    wp_register_style( 'slckwpGsTheme',  plugin_dir_url( __FILE__ ) . 'slick/slick/slick-theme.css',array('slickwpGs') );
+    wp_register_style( 'slckwpGsTheme',  plugin_dir_url( __FILE__ ) . 'slick/slick/slick-theme.css',array('slckwpGs') );
     wp_enqueue_script('slick_wp_gs_ms', plugin_dir_url(__FILE__).'slick/slick/slick.js', array('jquery'), 'version', true);
     wp_enqueue_style( 'slckwpGsTheme' );
     wp_enqueue_script('slick_wp_gs_ms');
