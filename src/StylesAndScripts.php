7abd5012e86632f494fce9663b690045c030c019
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 11:51:33 -0300

    add my css and js

--- src/StylesAndScripts.php
+++ src/StylesAndScripts.php
@@ -8,8 +8,10 @@ class StylesAndScripts extends Singleton{
 	public function myplugin_scripts(){
 		wp_register_style( 'slckwpGs',  SLICKWP_BASE_URL . 'slick/slick/slick.css' );
 		wp_register_style( 'slckwpGsTheme',  SLICKWP_BASE_URL . 'slick/slick/slick-theme.css',array('slckwpGs') );
+		wp_register_style( 'slickWpStyle',  SLICKWP_BASE_URL . 'css/slickwp.css',array('slckwpGsTheme') );
 		wp_enqueue_script('slick_wp_gs_ms', SLICKWP_BASE_URL.'slick/slick/slick.js', array('jquery'), 'version', true);
-		wp_enqueue_style( 'slckwpGsTheme' );
-		wp_enqueue_script('slick_wp_gs_ms');
+		wp_enqueue_script('slick_wp_js', SLICKWP_BASE_URL.'js/slickwp.js', array('slick_wp_gs_ms'), 'version', true);
+		wp_enqueue_style( 'slickWpStyle' );
+		wp_enqueue_script('slick_wp_js');
 	}
 }
