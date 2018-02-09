7eddd749d613f9203679de181e777526a06792df
Jorge Saud <jorgelsaud@gmail.com>
2018-02-09 09:58:15 -0300

    use singletons

--- src/StylesAndScripts.php
+++ src/StylesAndScripts.php
@@ -7,7 +7,7 @@ class StylesAndScripts{
 		add_action( 'wp_enqueue_scripts', array($this,'styles') );
 	}
 	public function styles(){
-				wp_register_style( 'slickWpGs', plugins_url( '/slick/slick/slick.css', __FILE__ ), array('jquery'), '1.8.0', 'all' );
+		wp_register_style( 'slickWpGs', plugins_url( '/slick/slick/slick.css', __FILE__ ), array('jquery'), '1.8.0', 'all' );
 		wp_enqueue_style( 'slickWpGs');
 
 	}
