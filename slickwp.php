40471c93e934b7a2c3fab8739de8e8e7f51f7e5e
Giorgiosaud <jorgelsaud@gmail.com>
2018-02-11 19:22:08 -0300

    include cmb2 to plugin

--- slickwp.php
+++ slickwp.php
@@ -18,7 +18,11 @@ if (!defined('SLICKWP_FILE')) {
 if (!defined('SLICKWP_BASE_URL')) {
     define('SLICKWP_BASE_URL', plugin_dir_url( __FILE__ ));
 }
-
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
 require_once 'vendor/autoload.php';
 function slickPlugin()
 {
