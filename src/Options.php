c34ccfc7980c808f48d8a907531c360f74192f1a
Giorgiosaud <jorgelsaud@gmail.com>
2018-02-11 18:35:18 -0300

    debug

--- src/Options.php
+++ src/Options.php
@@ -131,6 +131,9 @@ class Options
 
         return $new_input;
     }
+    public function sanitize_general_settings(){
+        die(var_dump('expression'));
+    }
 
     /** 
      * Print the Section text
